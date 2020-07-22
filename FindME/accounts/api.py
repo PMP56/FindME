32e14c9afc8f95d69000222fa2020ed057af8970
PMP56 <prashannapaudel2@gmail.com>
2020-07-22 21:26:41 +0545

    Login and logout api

--- FindME/accounts/api.py
+++ FindME/accounts/api.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from knox.models import AuthToken
-from .serializers import UserSerializer, RegisterSerializer
+from .serializers import UserSerializer, RegisterSerializer, LoginSerializer
 
 # RegisterAPI
 
@@ -17,3 +17,29 @@ class RegisterAPI(generics.GenericAPIView):
             "user": UserSerializer(user, context=self.get_serializer_context).data,
             "token": AuthToken.objects.create(user)[1]
         })
+
+# loginAPI
+
+
+class LoginAPI(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context).data,
+            "token": AuthToken.objects.create(user)[1]
+        })
+
+
+# GetUser API
+class UserAPI(generics.RetrieveAPIView):
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
--- FindME/accounts/serializers.py
+++ FindME/accounts/serializers.py
@@ -24,4 +24,15 @@ class RegisterSerializer(serializers.ModelSerializer):
             validated_data['username'], validated_data['email'], validated_data['password'])
         return user
 
-# User Serializer
+# Login Serializer
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Incorrect credentials")
