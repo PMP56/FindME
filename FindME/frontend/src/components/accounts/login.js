007b3e86520121efc4eedd454e8bb7c7f600c137
PMP56 <prashannapaudel2@gmail.com>
2020-07-25 00:23:00 +0545

    Basic frontend work

--- FindME/frontend/src/components/accounts/login.js
+++ FindME/frontend/src/components/accounts/login.js
@@ -1,10 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <h1>
-            This is Login Page.
-        </h1>
+        <Fragment>
+            <div className='pt-5 vh-100' style={{ backgroundColor: 'rgb(10, 10, 10)' }}>
+                <div className='col-md-4 m-auto' style={{ backgroundColor: 'rgb(20, 20, 20)' }}>
+                    <div className='card card-body mt-5 text-white' style={{ backgroundColor: 'rgb(30, 30, 30)' }}>
+                        <h2 className='text-center text-white'>Login</h2><br />
+                        <form>
+                            <div className='form-group pt-2'>
+                                <label>Email</label>
+                                <input className='border border-dark rounded form-control' name='email' placeholder='Email' />
+                            </div><br />
+                            <div className='form-group pt-2'>
+                                <label>Password</label>
+                                <input className='border border-dark rounded form-control' name='password' placeholder='Password' />
+                            </div><br />
+                            <div className='form-group pt-2 text-center'>
+                                <button type='submit' className='border border-white rounded btn btn-primary px-5 py-2'>Login</button>
+                            </div><br />
+                            <p className='text-center pr-3 text-muted'>Don't Have an Account?  <Link className='text-white fw-800' to='/register'>Register</Link> </p>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </Fragment>
     );
 }
 
--- FindME/frontend/src/components/accounts/register.js
+++ FindME/frontend/src/components/accounts/register.js
@@ -1,10 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <h1>
-            This is Register Page.
-        </h1>
+        <Fragment>
+            <div className='pt-5 vh-100' style={{ backgroundColor: 'rgb(10, 10, 10)' }}>
+                <div className='col-md-4 m-auto' style={{ backgroundColor: 'rgb(20, 20, 20)' }}>
+                    <div className='card card-body mt-5 text-white' style={{ backgroundColor: 'rgb(30, 30, 30)' }}>
+                        <h2 className='text-center text-white'>Register</h2><br />
+                        <form>
+                            <div className='form-group pt-2'>
+                                <label>Username</label>
+                                <input className='border border-dark rounded form-control' name='username' placeholder='Username' />
+                            </div>
+                            <div className='form-group pt-2'>
+                                <label>Email</label>
+                                <input className='border border-dark rounded form-control' name='email' placeholder='Email' />
+                            </div>
+                            <div className='form-group pt-2'>
+                                <label>Password</label>
+                                <input className='border border-dark rounded form-control' name='password' placeholder='Password' />
+                            </div>
+                            <div className='form-group pt-2'>
+                                <label>Password 2</label>
+                                <input className='border border-dark rounded form-control' name='password2' placeholder='Password Again' />
+                            </div>
+                            <div className='form-group pt-2 text-center'>
+                                <button type='submit' className='border border-white rounded btn btn-primary px-5 py-2'>Submit</button>
+                            </div><br />
+                            <p className='text-center pr-3 text-muted'>Already Have an Account?  <Link className='text-white fw-800' to='/login'>Login</Link> </p>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </Fragment>
+
     );
 }
 
