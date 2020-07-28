c18a5f4f4de0477ff0d3fc46b86d9f1d064addc1
PMP56 <prashannapaudel2@gmail.com>
2020-07-29 00:27:28 +0545

    For Heroku

--- FindME/FindME/settings.py
+++ FindME/FindME/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -138,3 +139,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'frontend'),  # update the STATICFILES_DIRS
 )
+
+django_heroku.settings(locals())
