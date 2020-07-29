c20b4480130d610f2c1ba6309d19ff5dbdfac6e3
PMP56 <prashannapaudel2@gmail.com>
2020-07-29 11:46:22 +0545

    Asgi

--- FindME/FindME/asgi.py
+++ FindME/FindME/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FindME.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FindME.FindME.settings')
 
 application = get_asgi_application()
