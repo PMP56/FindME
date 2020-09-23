829c899d75b61c02e9166fd57e71dd1cf767a6ed
SalinaK1 <saleena.koiralka1@gmail.com>
2020-09-23 12:32:05 +0545

    Heroku migration testing

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,5 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    is_employer = models.BooleanField('employer_status', default=False)
-    is_employee = models.BooleanField('employee_status', default=False)
-    is_admin = models.BooleanField('admin_status', default=False)
-    
+    class Meta:
+        db_table = 'auth_user'
