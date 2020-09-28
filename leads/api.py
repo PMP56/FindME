f3ab4714ae03b45c26d74a461c9469a5ff91b452
SalinaK1 <saleena.koirala1@gmail.com>
2020-09-28 22:37:00 +0545

    API to list messages created.

--- leads/api.py
+++ leads/api.py
@@ -1,5 +1,7 @@
 from leads.models import Lead
 from rest_framework import viewsets, permissions
+from django.db.models import Q
+from django.core import exceptions
 from .serializers import LeadSerializer
 
 # Lead Viewset
@@ -11,3 +13,26 @@ class LeadViewSet(viewsets.ModelViewSet):
         permissions.AllowAny
     ]
     serializer_class = LeadSerializer
+
+    def get_queryset(self):
+        inbox = self.request.query_params.get('inbox', None)
+        outbox = self.request.query_params.get('outbox', None)
+        message_to = self.request.query_params.get('message_to', None)
+        message_from = self.request.query_params.get('message_from', None)
+        try:
+            if inbox is not None:
+                queryset = self.queryset.filter(sent_to = inbox)
+            elif outbox is not None:
+                queryset = self.queryset.filter(sent_by = outbox)
+            elif message_to is not None and message_from is not None:
+                queryset = self.queryset.filter(Q(sent_to = message_to) & Q(sent_by = message_from)) | ( Q(sent_to = message_from) & Q(sent_by = message_to) )    
+            elif message_to is not None and message_from is None:
+                raise exceptions.PermissionDenied()
+            elif message_to is None and message_from is not None:
+                raise exceptions.PermissionDenied()
+            else:
+                queryset = self.queryset.all()
+        except:
+            queryset = []
+
+        return queryset
