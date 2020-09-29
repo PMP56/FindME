e6914a1f0d3c5c749ba4f00729216c003dfea4b5
SalinaK1 <saleena.koirala1@gmail.com>
2020-09-30 01:33:06 +0545

    My local error fixed.

--- FindME/urls.py
+++ FindME/urls.py
@@ -7,18 +7,6 @@ from drf_yasg import openapi
 
 
 schema_view = get_schema_view(
-<<<<<<< HEAD
-   openapi.Info(
-      title="Snippets API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-=======
     openapi.Info(
         title="Snippets API",
         default_version='v1',
@@ -29,7 +17,6 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
->>>>>>> f2d9685de78a6932ba37707d61ab662418aa8aae
 )
 
 urlpatterns = [
