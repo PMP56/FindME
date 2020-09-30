81872c4d0075aa7eef5510035ea9c6865f7c02f2
gbshakya <51392366+gbshakya@users.noreply.github.com>
2020-09-30 12:49:37 +0545

    about and little mods on contact
    about mods

--- frontend/src/components/layouts/landing.js
+++ frontend/src/components/layouts/landing.js
@@ -1,49 +1,54 @@
 import React, { useContext, Fragment } from "react";
 import { Route, Redirect, Switch, useParams } from "react-router-dom";
-import { AuthContext } from '../../utils/userContext';
+import { AuthContext } from "../../utils/userContext";
 
-import Header from './headers';
-import LandingBody from './landing_body';
-import Footer from './footer';
-import Contact from './contact';
-import Login from '../accounts/login';
-import Register from '../accounts/register';
-import NonEditProfile from '../user/nonEditProfile';
-
+import Header from "./headers";
+import LandingBody from "./landing_body";
+import Footer from "./footer";
+import Contact from "./contact";
+import Login from "../accounts/login";
+import Register from "../accounts/register";
+import NonEditProfile from "../user/nonEditProfile";
+import About from "./about.js";
 
 const LandingPage = () => {
-    const { currentUser } = useContext(AuthContext);
-    const { searchUser } = useParams();
-    if (currentUser) {
-        return <Redirect to="/" />;
-    }
-    return (
-        <Fragment>
-            <Route exact path="/">
-                <Header />
-                <LandingBody />
-                <Footer />
-            </Route>
+  const { currentUser } = useContext(AuthContext);
+  const { searchUser } = useParams();
+  if (currentUser) {
+    return <Redirect to="/" />;
+  }
+  return (
+    <Fragment>
+      <Route exact path="/">
+        <Header />
+        <LandingBody />
+        <Footer />
+      </Route>
 
-            <Switch>
-                <Route exact path="/contact">
-                    <Header />
-                    <Contact />
-                </Route>
-                <Route exact path="/login">
-                    <Header />
-                    <Login />
-                </Route>
-                <Route exact path="/register">
-                    <Header />
-                    <Register />
-                </Route>
-                <Route exact path="/:username">
-                    <NonEditProfile edit={false} />
-                </Route>
-            </Switch>
-        </Fragment>
-    );
-}
+      <Switch>
+        <Route exact path="/about">
+          <Header />
+          <About />
+        </Route>
+
+        <Route exact path="/contact">
+          <Header />
+          <Contact />
+        </Route>
+        <Route exact path="/login">
+          <Header />
+          <Login />
+        </Route>
+        <Route exact path="/register">
+          <Header />
+          <Register />
+        </Route>
+        <Route exact path="/:username">
+          <NonEditProfile edit={false} />
+        </Route>
+      </Switch>
+    </Fragment>
+  );
+};
 
 export default LandingPage;
